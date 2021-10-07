58063bcc082ed2fb629558e666188704988d3722
AzizTb <raphlust69@gmaul.com>
2021-10-07 13:20:18 +0700

    latihan template blade

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,19 @@ use App\Http\Controllers\DataSiswaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::get('album', function () {
-    return view('profile.album');
-}) -> name('album');
 
 Route::get('index', function () {
     return view('profile.index');
 }) -> name('index');
+
+Route::get('about', function () {
+    return view('profile.about');
+}) -> name('about');
+
+Route::get('post', function () {
+    return view('profile.post');
+}) -> name('post');
+
+Route::get('contact', function () {
+    return view('profile.contact');
+}) -> name('contact');
